cbf65e75b9a8e64ca98ead77150e301f589e61ca
Shahariar Rashid Fahim <fahimfahim450@gmail.com>
2023-10-22 02:47:49 +0600

    yml file configured

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -26,7 +26,6 @@ let clients = [
     {
         name : "Evan Oyster",
         position : "Entrepreneur",
-       
         img_url : "/assets/evan.png",
         stars : 5,
         disc : `Shahariar is a passionate and skilled software developer and Salesforce Admin. His work is always to customer specifications, looks very clean, and he excellent communication and collaboration skills greatly facilitated our team.
